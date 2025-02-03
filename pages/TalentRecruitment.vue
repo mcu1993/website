5b18e9bb2066178e20e5c01094bc3544c5d493c2
hzc <huangzc@topode.com>
2025-02-03 17:47:35 +0800

    123

--- pages/TalentRecruitment.vue
+++ pages/TalentRecruitment.vue
@@ -8,22 +8,21 @@
         </div>
     </div>
     <div class="body">
-        <div class="section">
-            <div class="common-title-text"  data-aos="fade-up">人才招聘</div>
-            <div class="join-idea-item" :style="{ 'background-image': footer_list[selectedIndex].img2 }"  data-aos="fade-up">
-                <div class="join-idea">
-                    <div class="join-idea-n">01</div>
-                    <div class="join-idea-p">
-                        企业之间的竞争，不仅仅是知识产品和服务的竞争，更是人才的竞争，好的人才将为企业的发展提供动力，因此力辰科技坚持“以人为本”和“事业留才”人才理念，引进“学历+技术”的优秀人才，同时不断优化人才成长环境，为人才的成长提供广阔的平台，建立“外引内培”的人员储备渠道。
+        <div class="section" data-aos="fade-up">
+            <div class="common-title-text">人才招聘</div>
+            <div class="invite-text">科睿高科认真关注每一位员工的成长！</div>
+            <div class="invite-p">
+                在科睿高科，有一整套科学的人才招聘、选拔、使用和开发的人力资源管理体系。我们有广阔的个人发展空间、优良的个人成长环境、有效的绩效考核激励机制、丰富的企业文化和优厚的福利待遇。目前热招职位如下：</div>
+            <div class="invite-item">
+                <div class="invite-item-box">
+                    <div class="invite-shrink">
+                        <img :src="img14" alt="" class="shrink-img1">
+                        <img :src="img15" alt="" class="shrink-img2">
                     </div>
-                </div>
-            </div>
-            <div class="join-idea-item" :style="{ 'background-image': footer_list[selectedIndex].img3 }"  data-aos="fade-up">
-                <div class="join-idea">
-                    <div class="join-idea-n">02</div>
-                    <div class="join-idea-p">
-                        以“优化人才梯队结构、提高人才队伍素质”为核心，以“知识化、专业化、职业化”为发展策略，重点打造高素质的专业人才、高水平的技能人才、复合型管理人才队伍，让人才发展与企业发展齐头共进。
+                    <div class="invite-item-icon">
+                        <img :src="img16" alt="">
                     </div>
+                    <div class="invite-item-title">电商运营店长/主管</div>
                 </div>
             </div>
         </div>
@@ -41,6 +40,10 @@ const { t } = useI18n();
 import img11 from "@/assets/talent/talent-2.jpg";
 import img12 from "@/assets/talent/talent-3.jpg";
 import img13 from "@/assets/talent/talent-4.jpg";
+import img14 from "@/assets/talent/invite-right.png";
+import img15 from "@/assets/talent/invite-right.png";
+import img16 from "@/assets/talent/invite-icon.png";
+
 const footer_list = [
     {
         bannerTitle: '携手科睿高科',
@@ -90,6 +93,7 @@ onBeforeUnmount(() => {
     .banner_tips {
         margin: auto;
         display: flex;
+
         .banner_title {
             font-size: clamp(16px, 4vw, 28px);
             color: #fff;
@@ -123,6 +127,7 @@ onBeforeUnmount(() => {
         display: flex;
         flex-direction: column;
         align-items: start;
+        padding: 0 20px;
 
         .common-title-text {
             font-size: 36px;
@@ -130,45 +135,83 @@ onBeforeUnmount(() => {
             font-weight: bold;
             color: #333333;
             line-height: 44px;
-            margin-bottom: 30px;
         }
 
-        .join-idea-item {
-            width: 100%;
-            height: auto;
-            padding: 50px 127px;
-            background-repeat: no-repeat;
-            background-size: cover;
-            margin-bottom: 24px;
+        .invite-text {
+            margin: 24px 0;
+            font-size: 19px;
+            font-family: Microsoft YaHei-Bold, Microsoft YaHei;
+            font-weight: bold;
+            color: #333333;
+        }
 
-            .join-idea {
-                padding: 24px;
-                background: rgba(255, 255, 255, 0.35);
-                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1), inset 0 1px 2px 0 rgba(222, 243, 255, 0.4);
-                border-radius: 0;
-                opacity: 1;
-                border: 1px solid;
-                border-image: linear-gradient(180deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 1)) 0.01 0.01;
-                /* filter: blur(1px); */
+        .invite-p {
+            font-size: 16px;
+            font-family: Microsoft YaHei-Regular, Microsoft YaHei;
+            font-weight: 400;
+            color: #333333;
+            line-height: 34px;
+            margin-bottom: 48px;
+        }
+
+        .invite-item {
+            width: 100%;
+            padding: 24px 48px;
+            background: #FFFFFF;
+            border-radius: 5px 5px 5px 5px;
+            opacity: 1;
+            border: 1px solid #DDDDDD;
+            transition: all .365s;
+            position: relative;
+            margin-bottom: 30px;
+
+            .invite-item-box {
                 display: flex;
                 align-items: center;
+                cursor: pointer;
 
-                .join-idea-n {
-                    font-size: 36px; 
-                    color: #ecf0f1;
-                    margin-right: 10px;
+                .shrink-img1 {
+                    width: 9px;
+                    height: 18px;
                 }
 
-                .join-idea-p {
-                    font-size: 18px;
-                    text-align: justify;
-                    font-family: Microsoft YaHei-Regular, Microsoft YaHei;
-                    font-weight: 400;
-                    color: #FFFFFF;
-                    line-height: 36px;
+                .shrink-img2 {
+                    width: 18px;
+                    height: 9px;
+                    display: none;
+                }
+
+                .invite-item-icon {
+                    width: 28px;
+                    height: 28px;
+                    flex-shrink: 0;
+                    margin: 0 15px;
+                }
+
+                .invite-item-title {
+                    font-size: 21px;
+                    font-family: Microsoft YaHei-Bold, Microsoft YaHei;
+                    font-weight: bold;
+                    color: #333333;
                 }
             }
+        }
 
+        .invite-item-active {
+            box-shadow: 0 0 24px 0 rgba(40, 65, 134, 0.15);
+            border-radius: 5px 5px 5px 5px;
+            opacity: 1;
+            border-bottom: 3px solid #94C33A;
+            padding: 40px 48px;
+
+            .shrink-img1 {
+                display: none;
+            }
+
+            .shrink-img2 {
+                display: block;
+                transition: all .365s;
+            }
         }
     }
 }
@@ -202,58 +245,21 @@ onBeforeUnmount(() => {
 
     .body {
         .section {
-        margin: auto;
-        margin-bottom: 4vw;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        .common-title-text {
-            font-size: 4.8vw;
-            font-family: Microsoft YaHei-Bold, Microsoft YaHei;
-            font-weight: bold;
-            color: #333333;
-            line-height: 5.867vw;
+            margin: auto;
             margin-bottom: 4vw;
-        }
+            display: flex;
+            flex-direction: column;
+            align-items: center;
 
-        .join-idea-item {
-            width: 100%;
-            height: auto;
-            padding: 6.667vw 6.933vw;
-            background-repeat: no-repeat;
-            background-size: cover;
-            margin-bottom: 3.2vw;
-
-            .join-idea {
-                padding: 3.2vw;
-                background: rgba(255, 255, 255, 0.35);
-                box-shadow: 0 0.267vw 0.267vw 0 rgba(0, 0, 0, 0.1), inset 0 0.133vw 0.267vw 0 rgba(222, 243, 255, 0.4);
-                border-radius: 0;
-                opacity: 1;
-                border: 1px solid;
-                border-image: linear-gradient(180deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 1)) 0.01 0.01;
-                /* filter: blur(1px); */
-                display: flex;
-                align-items: center;
-
-                .join-idea-n {
-                    font-size: 4.8vw; 
-                    margin-right: 1.333vw;
-                }
-
-                .join-idea-p {
-                    font-size: 2.4vw;
-                    text-align: justify;
-                    font-family: Microsoft YaHei-Regular, Microsoft YaHei;
-                    font-weight: 400;
-                    color: #FFFFFF;
-                    line-height: 4.8vw;
-                }
+            .common-title-text {
+                font-size: 4.8vw;
+                font-family: Microsoft YaHei-Bold, Microsoft YaHei;
+                font-weight: bold;
+                color: #333333;
+                line-height: 5.867vw;
+                margin-bottom: 4vw;
             }
-
         }
-    }
     }
 }
 </style>
